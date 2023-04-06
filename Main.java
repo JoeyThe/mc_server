38927c46214303935f24b78de873f39655bc093a
joey <joey@joey.com>
2023-04-05 22:38:43 -0700

    Re-coded LogReader

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 import reader.LogReader;
-import commander.Commander;
+//import commander.Commander;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		LogReader logReader = new LogReader("/home/joey/mc_server/logs/latest.log");
-		Commander Commander = new Commander();
-		logReader.print_logs();
+//		Commander Commander = new Commander();
+//		logReader.print_logs();
 
 		logReader.continuous_read();
 	}
--- reader/LogReader.java
+++ reader/LogReader.java
@@ -1,5 +1,9 @@
 package reader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LogReader
 {
 	// Attributes
@@ -10,6 +14,37 @@ public class LogReader
 	{
 		this.path = path;
 		System.out.println("Path: "+path);
+	}
+
+	public void continuous_read()
+	{
+		BufferedReader reader;
+
+		try {
+			reader = new BufferedReader(new FileReader(this.path));
+			String line = reader.readLine();
+
+			while (true) {
+				if (line != null) {
+					System.out.println(line);
+					if (line.contains("shit")) {
+						System.out.println("No cursing on the server");
+						break;
+					}
+				}
+				line = reader.readLine();
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
